83d070a01f82dbca75fbf4e7bdac3652604d290e
FelixRilling <contact@rilling.dev>
2021-03-16 10:07:38 +0100

    Allow fieldset as element.

--- src/dom/ValidatableElement.ts
+++ src/dom/ValidatableElement.ts
@@ -3,9 +3,10 @@
  * The shared method of doing so is the respective #setCustomValidity(string) method.
  *
  * Some other elements also have such a method but are not suitable for Ok, such as
- * {@link HTMLFieldSetElement}, {@link HTMLObjectElement} and {@link HTMLButtonElement}.
+ * {@link HTMLObjectElement} and {@link HTMLButtonElement}.
  */
 export type ValidatableElement =
     | HTMLInputElement
     | HTMLTextAreaElement
-    | HTMLSelectElement;
+    | HTMLSelectElement
+    | HTMLFieldSetElement;
